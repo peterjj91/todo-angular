8d528d8de6db065ae2ccfa7a067d2e2021aa909c
Popeskul Pavel <peterjj91@gmail.com>
2019-04-12 19:01:41 +0300

    List, NotFound, New, Routing setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { NewComponent } from './new/new.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    NewComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
